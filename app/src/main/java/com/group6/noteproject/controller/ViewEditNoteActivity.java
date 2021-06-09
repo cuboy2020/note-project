f0dbac5defb35e1ab79dd0214c80c15836919d18
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-06-09 21:20:13 +0700

    Partial View Note

---
+++ app/src/main/java/com/group6/noteproject/controller/ViewEditNoteActivity.java
@@ -0,0 +1,21 @@
+package com.group6.noteproject.controller;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group6.noteproject.R;
+
+public class ViewEditNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_note);
+    }
+
+    public void onClick(View v){
+        
+    }
+}

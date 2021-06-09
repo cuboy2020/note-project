767083dd8bee0cd306d10a93e7c722ebf6c57976
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-06-09 22:35:58 +0700

    Main View and Item View

---
+++ app/src/main/java/com/group6/noteproject/controller/MainActivity.java
@@ -0,0 +1,17 @@
+package com.group6.noteproject.controller;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group6.noteproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

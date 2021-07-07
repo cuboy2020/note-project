f80792a006a1542002bb9b0d464b219b772d7f46
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2021-07-07 16:59:07 +0700

    Rewrite whole app

--- app/src/main/java/com/group6/noteproject/util/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.group6.noteproject.util;
-
-public class Constant {
-    public final static String DATABASE_NAME = "note.db";   // database's name
-    public final static String ACCOUNT_KEY = "account.key"; // account's key
-}

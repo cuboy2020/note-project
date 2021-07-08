e2ec26ebd2f08b4c5effe08f07d03f31c4286cb0
BpHana <57785153+BpHana@users.noreply.github.com>
2021-07-08 11:15:14 +0700

    register 02

--- app/src/main/java/com/group6/noteapp/controller/RegisterFragment02.java
+++ app/src/main/java/com/group6/noteapp/controller/RegisterFragment02.java
@@ -3,23 +3,19 @@ package com.group6.noteapp.controller;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.button.MaterialButton;
 import com.group6.noteapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment01#newInstance} factory method to
+ * Use the {@link RegisterFragment02#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RegisterFragment01 extends Fragment {
-
-    View inflatedView;
+public class RegisterFragment02 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +26,7 @@ public class RegisterFragment01 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RegisterFragment01() {
+    public RegisterFragment02() {
         // Required empty public constructor
     }
 
@@ -40,11 +36,11 @@ public class RegisterFragment01 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
+     * @return A new instance of fragment RegisterFragment02.
      */
     // TODO: Rename and change types and number of parameters
-    public static RegisterFragment01 newInstance(String param1, String param2) {
-        RegisterFragment01 fragment = new RegisterFragment01();
+    public static RegisterFragment02 newInstance(String param1, String param2) {
+        RegisterFragment02 fragment = new RegisterFragment02();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,14 +61,6 @@ public class RegisterFragment01 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        inflatedView = inflater.inflate(R.layout.fragment_register01, container, false);
-        MaterialButton btnNext = inflatedView.findViewById(R.id.btnNext01);
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                NavHostFragment.findNavController(RegisterFragment01.this).navigate(R.id.action_registerFragment01_to_registerFragment02);
-            }
-        });
-
-        return inflatedView;
+        return inflater.inflate(R.layout.fragment_register02, container, false);
     }
 }

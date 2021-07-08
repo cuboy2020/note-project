1d979cc90868180e2c49007af70a9474f7b29af3
BpHana <57785153+BpHana@users.noreply.github.com>
2021-07-08 10:23:57 +0700

    register add guideline to login

--- app/src/main/java/com/group6/noteapp/controller/RegisterFragment01.java
+++ app/src/main/java/com/group6/noteapp/controller/RegisterFragment01.java
@@ -3,23 +3,19 @@ package com.group6.noteapp.controller;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.button.MaterialButton;
 import com.group6.noteapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link RegisterFragment01#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
-
-    View inflatedView;
+public class RegisterFragment01 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +26,7 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment() {
+    public RegisterFragment01() {
         // Required empty public constructor
     }
 
@@ -40,17 +36,15 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment RegisterFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static RegisterFragment01 newInstance(String param1, String param2) {
+        RegisterFragment01 fragment = new RegisterFragment01();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
-
-
         return fragment;
     }
 
@@ -67,17 +61,6 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        inflatedView = inflater.inflate(R.layout.fragment_login, container, false);
-
-        // Get register button
-        MaterialButton btnRegister = inflatedView.findViewById(R.id.btnRegister);
-        // Set navigate to register button
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.action_loginFragment_to_registerFragment01);
-            }
-        });
-        return inflatedView;
+        return inflater.inflate(R.layout.fragment_register01, container, false);
     }
-
 }
